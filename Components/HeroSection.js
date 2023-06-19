74bf49a783b0d7304a9d9e25597680865585bac4
arhanz <96288469+arhanz@users.noreply.github.com>
2023-06-19 15:45:23 +0500

    add files

---
+++ Components/HeroSection.js
@@ -0,0 +1,12 @@
+import SLiderImg1 from "../Assets/hero_bg_1.jpg"
+import SLiderImg2 from "../Assets/hero_bg_2.jpg"
+import SLiderImg3 from "../Assets/hero_bg_3.jpg"
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+     
+    </>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import HeroSection from "../Components/HeroSection"
+
+export default function Home() {
+  return (    
+    <>
+    <h1>Welcome TO LandingPage</h1>
+    <HeroSection />
+    </>
+  )
+}
